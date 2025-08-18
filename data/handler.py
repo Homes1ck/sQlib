2fc154a2bc29d96ff7f2b543d3ca6b1f353099a7
Homes1ck <527457321@qq.com>
2025-08-18 22:44:36 +0800

    0818

--- data/handler.py
+++ data/handler.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+import re
 from tqdm import tqdm
 from copy import deepcopy
 
@@ -21,20 +22,24 @@ class Handler():
     DK_I = "infer"
     DK_L = "learn"
 
-    def _labelfunc(self, df_origin, label, start_time, end_time):
-
-        df = pd.DataFrame()
-
-        df["日期"] = df_origin["日期"]
-        df["股票代码"] = df_origin["股票代码"]
-
+    def _labelfunc(self, df_origin, label):
+        
+        '''
         for col in df_origin.columns:
             label = label.replace(f"[{col}]", f'df_origin["{col}"]')
-        df["LABEL0"] = eval(label)
+        
+        df_origin["LABEL0"] = eval(label)
+        '''
 
-        df = df[(df["日期"] >= start_time) & (df["日期"] <= end_time)]
+        match = re.search(r'shift\((-?\d+)\)', label)
+        if match:
+            shift = int(match.group(1))
 
-        return df
+            with np.errstate(divide='ignore', invalid='ignore'):
+                df_origin["LABEL0"] = np.log(df_origin["收盘"].shift(shift)) - np.log(df_origin["收盘"])
+            
+            #df_origin.loc[df_origin["收盘"] <= 0, "LABEL0"] = np.nan
+            #df_origin.loc[df_origin["收盘"].shift(shift) <= 0, "LABEL0"] = np.nan
 
     def prepare(self,
         key=None,      
@@ -58,73 +63,70 @@ class Handler():
         all_factor = []
         all_label = []
 
-        append_actions = []
-
-        # factor（feature）
-        if "feature" in col_set:
-            def append_feature(code, left_time, right_time):
-                factor_path = f"cn_data/factor/{code}.parquet"
-                df_factor = pd.read_parquet(factor_path)
-                df_factor['日期'] = pd.to_datetime(df_factor['日期'])
-                df_factor_trunc = df_factor[(df_factor["日期"] >= left_time) & (df_factor["日期"] <= right_time)]
-                all_factor.append(df_factor_trunc)
-            append_actions.append(append_feature)
-
-        # origin（raw）
-        if "raw" in col_set:
-            def append_raw(code, left_time, right_time):
-                origin_path = f"cn_data/origin/{code}.parquet"
-                df_origin = pd.read_parquet(origin_path)
-                df_origin['日期'] = pd.to_datetime(df_origin['日期'])
-                df_origin_trunc = df_origin[(df_origin["日期"] >= left_time) & (df_origin["日期"] <= right_time)]
-                all_origin.append(df_origin_trunc)
-            append_actions.append(append_raw)
-
-        # label
-        if "label" in col_set:
-            def append_label(code, left_time, right_time):
-                origin_path = f"cn_data/origin/{code}.parquet"
-                df_origin = pd.read_parquet(origin_path)
-                df_origin['日期'] = pd.to_datetime(df_origin['日期'])
-                df_origin_trunc = df_origin[(df_origin["日期"] >= left_time) & (df_origin["日期"] <= right_time + pd.Timedelta(days=10))]
-                df_label = self._labelfunc(
-                    df_origin=df_origin_trunc,
-                    label=self.label,
-                    start_time=left_time,
-                    end_time=right_time
-                )
-                all_label.append(df_label)
-            append_actions.append(append_label)
-
         instruments = self.instruments
+
+        ops = []
+        if "label" in col_set:
+            def append_label():
+                self._labelfunc(df_origin, self.label)
+                all_label.append(df_origin.loc[masko, ["日期", "股票代码", "LABEL0"]].copy())
+            ops.append(append_label)
+
+        if "feature" in col_set:
+            def append_feature():
+                all_factor.append(df_factor.loc[maskf].copy())
+            ops.append(append_feature)
+
+        if "raw" in col_set:
+            def append_raw():
+                all_origin.append(df_origin.loc[masko, df_origin.columns[:-1]].copy())
+            ops.append(append_raw)
 
         if isinstance(instruments, str):
             if  instruments in ["all", "csi100", "csi300", "csi500", "csi800", "csi1000", "csiall"]:   
                 instruments = pd.read_csv(f"instruments/{instruments}.txt", sep='\t', header=None)
                 instruments.columns = ["品种代码", "纳入日期", "剔除日期"]
-                instruments = instruments.set_index(['纳入日期', '剔除日期'])
+                
+                period_map = {
+                    code: group[["纳入日期", "剔除日期"]].values.tolist()
+                    for code, group in instruments.groupby("品种代码")
+                }
 
-                grouped = list(instruments.groupby(level=[0, 1]))
-                total = len(grouped)
+                for code, periods in period_map.items():
+                    code = code.lower()
 
-                for j, ((in_time, out_tiem), group) in enumerate(tqdm(grouped)):
-                    codes = group['品种代码'].tolist()
-                    in_time = pd.to_datetime(in_time)
-                    out_tiem = pd.to_datetime(out_tiem)
+                    origin_path = f"cn_data/origin/{code}.parquet"
+                    factor_path = f"cn_data/factor/{code}.parquet"
 
-                    if (out_tiem >= start_time and in_time <= end_time):
+                    df_origin = pd.read_parquet(origin_path)
+                    df_origin['日期'] = pd.to_datetime(df_origin['日期'])
+                    
+                    df_factor = pd.read_parquet(factor_path)
+                    df_factor['日期'] = pd.to_datetime(df_factor['日期'])
 
-                            left_time = max(in_time, start_time)
-                            right_time = min(out_tiem, end_time)
-                            
-                            if j == total - 1:
-                                right_time = end_time
+                    masko = pd.Series(False, index=df_origin.index)
+                    maskf = pd.Series(False, index=df_factor.index)
 
-                            for code in codes:
-                                code = code.lower()
+                    total = len(periods)
+                    for j, in_and_out in enumerate(periods):
 
-                                for action in append_actions:
-                                    action(code, left_time, right_time)
+                        in_time, out_time = in_and_out[0], in_and_out[1]
+
+                        in_time = pd.to_datetime(in_time)
+                        out_time = pd.to_datetime(out_time)
+
+                        start = max(in_time, start_time)
+                        end = min(out_time, end_time)
+
+                        if j == total - 1 and key == "test":
+                                end = end_time
+
+                        if start <= end:  # 有交集才保留
+                            masko |= df_origin["日期"].between(start, end)
+                            maskf |= df_factor["日期"].between(start, end)
+
+                    for op in ops:
+                        op()
 
                 append_map = {
                     "raw": all_origin,
@@ -141,7 +143,10 @@ class Handler():
                 if "label" in col_set:
 
                     mask = ~np.isfinite(np.array(df["label"]))
-                    df = df[~mask] 
+                    mask |= np.array(df["label"]) >= 0.2
+                    mask |= np.array(df["label"]) <= -0.2
+
+                    df = df[~mask]
 
                     mean = df["label"].mean()
                     std = df["label"].std()
@@ -149,6 +154,4 @@ class Handler():
                     df["label"] -= mean
                     df["label"] /= std
                 
-            return  df
-
-    
+                return  df
